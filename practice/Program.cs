cfc5d890ec32c7255dd8dd22659e094afbf27081
张子涵 <zhangzihan@s-MacBook-Air.local>
2025-07-17 15:14:29 +1200

    new submit

--- practice/Program.cs
+++ practice/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace practice;
+﻿namespace practice;  // 命名空间
 
 class Program
 {
+    // Main 程序入口
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("初次学习C#, 请多关照！");
+        Console.Write("时间");
+        Console.ReadKey();
+        Console.Write("过得好快");
+
+        Console.Read();
+        Console.WriteLine("继续运行");
+        
     }
 }
---
+++ variable_and_type/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace variable_and_type;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
